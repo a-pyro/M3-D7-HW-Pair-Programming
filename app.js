193de4e23d874742734f20452cb283f9b66ed337
Paul Maliakel <paulbalu93@gmail.com>
2021-03-16 21:12:55 +0530

    adds search

--- app.js
+++ app.js
@@ -2,21 +2,21 @@
 
 console.log('Hi there! 🔥');
 const shit = '💩',
-  fire = '🔥',
-  rocket = '🚀',
-  poudzo = '👍🏻';
+	fire = '🔥',
+	rocket = '🚀',
+	poudzo = '👍🏻';
 
 const userersEndpoint = 'https://jsonplaceholder.typicode.com/users';
 const robotSrc = 'https://robohash.org/';
 const CardComponent = ({
-  name,
-  id,
-  phone,
-  username,
-  email,
-  website,
-  company: { name: companyName, catchPhrase, bs },
-  address: { city, street, suite, zipcode, geo },
+	name,
+	id,
+	phone,
+	username,
+	email,
+	website,
+	company: { name: companyName, catchPhrase, bs },
+	address: { city, street, suite, zipcode, geo },
 }) => `
 <div class="col-6 mb-2">
             <div class="card">
@@ -81,20 +81,38 @@ const CardComponent = ({
 `;
 
 const renderUsers = (data) => {
-  const row = document.getElementById('mainRow');
-  row.innerHTML = data.reduce((acc, cv) => acc + CardComponent(cv), '');
+	const row = document.getElementById('mainRow');
+	row.innerHTML = data.reduce((acc, cv) => acc + CardComponent(cv), '');
 };
-
+let users = [];
 const getUsers = async (endpoint) => {
-  try {
-    const respone = await fetch(endpoint);
-    const data = await respone.json();
-    console.log(data);
-
-    renderUsers(data);
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const respone = await fetch(endpoint);
+		const data = await respone.json();
+		console.log(data);
+		users = [...data];
+		renderUsers(users);
+	} catch (error) {
+		console.log(error);
+	}
 };
+
+const searchUser = (e) => {
+	e.preventDefault();
+	const option = document.querySelector('.custom-select');
+	const optionValue = option.value;
+	// console.log(optionValue)
+	const search = document.querySelector('#searchbox').value.toLowerCase();
+	console.log(search);
+	console.log(optionValue);
+	const userNames = users.filter((user) => user[optionValue].toLowerCase().includes(search.toLowerCase()));
+
+	console.log(userNames);
+	renderUsers(userNames);
+};
+const form = document.querySelector('.form-inline');
+form.addEventListener('submit', searchUser);
+
+console.log(form);
 
 window.onload = getUsers(userersEndpoint);
